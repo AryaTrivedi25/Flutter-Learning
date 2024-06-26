39651e42cab24f182065575af2b5db88af892916
Arya Trivedi <trivediarya25@gmail.com>
2024-06-26 18:25:24 +0530

    Commit

--- Udemy/meals_app/lib/screens/categories.dart
+++ Udemy/meals_app/lib/screens/categories.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget{
   @override
@@ -8,24 +11,19 @@ class CategoriesScreen extends StatelessWidget{
       appBar: AppBar(
         centerTitle: true,
         title: Text("Pick your category !", style: TextStyle(color: Colors.black),),
-        backgroundColor: Colors.orangeAccent,
+        backgroundColor: Colors.yellow,
       ),
       body: GridView(
+        padding: EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3/2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: [
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),),
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),),
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),),
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),),
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),),
-          Text("1\n2\n3\n4\n5", style: TextStyle(color: Colors.white),)
-        ],
-      ),
+        children : availableCategories.map((category) {
+          return CategoryGridItem(category: category);
+        }).toList())
     );
   }
 
