35e2beab0feb24f814fe57343d48f487799b978d
Arya Trivedi <trivediarya25@gmail.com>
2024-06-28 16:49:30 +0530

    Commit

---
+++ Udemy/meals_app/lib/screens/meal_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MealDetails extends StatelessWidget{
+  const MealDetails({super.key});
+
+  // final List<dummyMeals> meal;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // title: meal.title,
+      ),
+    );
+  }
+
+}
--- Udemy/meals_app/lib/widgets/meal_item.dart
+++ Udemy/meals_app/lib/widgets/meal_item.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 import '../models/meal.dart';
 class MealItem extends StatelessWidget{
   const MealItem({super.key, required this.meal});
 
   final Meal meal;
+  String get affordabilityText {
+    return (meal.affordability.name[0].toUpperCase() +
+            meal.affordability.name.substring(1));
+  }
+
+  String get complexityText{
+    return (meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1));
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
     return Card(
       margin: const EdgeInsets.all(8.00),
       shape: RoundedRectangleBorder(
@@ -48,16 +60,19 @@ class MealItem extends StatelessWidget{
                   ),
                   const SizedBox(height: 12,),
                   Row(
-                    children: [],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MealItemTrait(icon: Icons.schedule, label: '${meal.duration} min'),
+                      const SizedBox(width: 12,),
+                      MealItemTrait(icon: Icons.work, label: complexityText),
+                      const SizedBox(width: 12,),
+                      MealItemTrait(icon: Icons.currency_rupee, label: affordabilityText),
+                    ],
                   )
                 ]
-
-
               ),
           ),
-
           )
-
         ],
       ),
     ),);
