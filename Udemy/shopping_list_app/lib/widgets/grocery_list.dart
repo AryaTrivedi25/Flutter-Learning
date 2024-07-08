de0b70eaab773fcdcb023570b2332f1dcb87de24
Arya Trivedi <trivediarya25@gmail.com>
2024-07-08 10:32:46 +0530

    commit

--- Udemy/shopping_list_app/lib/widgets/grocery_list.dart
+++ Udemy/shopping_list_app/lib/widgets/grocery_list.dart
@@ -6,7 +6,6 @@ class GroceryList extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title : const Text("Your Groceries"),
